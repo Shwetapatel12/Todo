c37e594e72719f9550a7430ed740f1424adb18d3
shweta_P <shwetapatel99585@gmail.com>
2025-01-07 17:19:43 +0530

    Initial commit with project files

---
+++ src/main/java/com/example/todo/TodoService.java
@@ -0,0 +1,40 @@
+package com.example.todo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Todo getTodoById(Long id) {
+            return todoRepository.findById(id).orElseThrow(() -> new RuntimeException("Todo not found"));
+    }
+
+    public Todo createTodo(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    public Todo updateTodo(Long id, Todo todoDetails) {
+        Todo todo = getTodoById(id);
+        todo.setTitle(todoDetails.getTitle());
+        todo.setCompleted(todoDetails.isCompleted());
+        return todoRepository.save(todo);
+    }
+
+    public void deleteTodoById(Long id) {
+        todoRepository.deleteById(id);
+    }
+
+	public Todo addTodo(Todo todo) {
+		
+		return todoRepository.save(todo);
+	}
+}
